9ca0b856011f0db01d1f8cc45652f7f9b3fad855
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-04-19 20:44:47 -0400

    Removed gl bindings in sphere

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -2,17 +2,6 @@
 
 #include <limits>
 
-// Included files for OpenGL Rendering
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <GLUT/glut.h>
-#else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glut.h>
-#endif
-
 // ====================================================================
 // ====================================================================
 
